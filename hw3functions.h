5d214496c6093358a1d51d2906ea1f7dc91840e5
goku-ai <63135653+goku-ai@users.noreply.github.com>
2020-04-17 00:35:12 -0500

    Minor fix

--- hw3functions.h
+++ hw3functions.h
@@ -26,22 +26,21 @@ void getRational(int* num, int* den){
     }
 }
 void reduce(int* num, int* den){
-int r, gcd , a ,b;
-a = *num;
-b = *den;
-if (a < 0){
-    a *= -1;
-}
-r = (a %b);
-while(r != 0){
-a = b;
-b =r;
-r =a % b;
-
-}
-gcd = b;
-*num /= gcd;
-*den /= gcd;
+    int r, gcd , a ,b;
+    a = *num;
+    b = *den;
+    if (a < 0){
+        a *= -1;
+    }
+    r = (a %b);
+    while(r != 0){
+        a = b;
+        b =r;
+        r =a % b;
+    }
+    gcd = b;
+    *num /= gcd;
+    *den /= gcd;
 }
 void AddRational(int* anum, int* aden, int num1, int den1, int num2, int den2){
     *anum = (num1 * den2)+ (num2 * den1);
@@ -56,16 +55,15 @@ void SubtractRational(int *anum, int * aden, int num1, int den1, int num2, int d
 }
 
 
-void DisplayRational(int num, int den)
-    {
-if (den == 1){
-    cout << num << endl;
+void DisplayRational(int num, int den){
+    if (den == 1){
+        cout << num << endl;
 }
-else if (num == 0){
-    cout << num << endl;
+    else if (num == 0){
+        cout << num << endl;
 }
-else{
-cout << num << "/" << den << endl;
+    else{
+        cout << num << "/" << den << endl;
 }
 }
 
@@ -74,12 +72,12 @@ void add(){
     int num1, den1, num2, den2, anum, aden;
     while(true){
     cout << "Addition of rational numbers" << endl;
-    getRational(&num1, &den1);
-    getRational(&num2, &den2);
-    AddRational(&anum, &aden, num1, den1, num2, den2);
+        getRational(&num1, &den1);
+        getRational(&num2, &den2);
+        AddRational(&anum, &aden, num1, den1, num2, den2);
     cout << "The result of ";
     cout << num1 <<"/"<<den1 <<" + " << num2 << "/" <<den2 << " = ";
-    DisplayRational(anum, aden);
+        DisplayRational(anum, aden);
     char user = '\0';
     cout << "Do you want to do more additions? (Y/N):";
     cin >> user; cin.ignore();
@@ -92,12 +90,12 @@ void subtract(){
     int num1, den1, num2, den2, anum, aden;
     while(true){
     cout << "Subtraction of rational numbers" << endl;
-    getRational(&num1, &den1);
-    getRational(&num2, &den2);
-    SubtractRational(&anum, &aden, num1, den1, num2, den2);
+        getRational(&num1, &den1);
+        getRational(&num2, &den2);
+        SubtractRational(&anum, &aden, num1, den1, num2, den2);
     cout << "The result of " ;
     cout << num1 <<"/"<<den1 <<" - " << num2 << "/" <<den2 << " = ";
-    DisplayRational(anum, aden);
+        DisplayRational(anum, aden);
     char user = '\0';
     cout << "Do you want to do more subtractions? (Y/N): ";
     cin >> user; cin.ignore();
